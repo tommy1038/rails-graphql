7d26d670c62d1ad76472e049c314d0ee9efde212
Ryota Tomiyama <lit.karu.noir@gmail.com>
2021-06-01 23:51:19 +0900

    query and query_resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,15 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    field :posts, [Types::PostType], null: false
+    def posts
+      Post.all
+    end
+    field :post, Types::PostType, null: false do
+      argument :id, Int, required: false
+    end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def post(id:)
+      Post.find(id)
     end
   end
 end
